01c8a74339a854c7aabfeca4f53f348541b4fd1f
Reward Khe <rewardkhe@outlook.com>
2024-02-21 12:26:01 +1100

    vite remodelled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import xmlogo from './assets/xm.png'
 import './App.css'
 
 function App() {
@@ -9,25 +10,25 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://github.com/rewardkhe/xero728/" target="_blank">
+          <img src={xmlogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        {/* <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + React</h1>
+      <h1>XeroMate</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click me  {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {/* Edit <code>src/App.jsx</code> and save to test HMR */}
         </p>
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
