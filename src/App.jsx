a13a14f101a54c94c36d262d8736880e36c3a0ac
Reward Khe <rewardkhe@outlook.com>
2024-02-23 10:29:54 +1100

    connect button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import xmlogo from './assets/xm.png'
 import './App.css'
 
@@ -22,6 +20,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Click me  {count}
         </button>
+        {/* <button onClick={()=> }>
+
+        </button> */}
         <p>
           {/* Edit <code>src/App.jsx</code> and save to test HMR */}
         </p>
---
+++ src/ConnectToXeroButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ConnectToXeroButton() {
+  const handleConnectToXero = () => {
+    const clientId = 'EB99B7C7A5B6431A801C4D0FE4D56ED1';
+    const redirectUri = encodeURIComponent('https://pilkadaris.com.au/');
+    const scope = encodeURIComponent('openid profile email offline_access accounting.transactions accounting.contacts');
+    const state = 'YOUR_STATE'; // Generate a unique state value for each authorization request
+    const authorizationUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}`;
+
+    window.location.href = authorizationUrl;
+  };
+
+  return (
+    <button onClick={handleConnectToXero}>Connect to Xero</button>
+  );
+}
+
+export default ConnectToXeroButton;
