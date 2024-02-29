660851713c0b85003e08d21b1b632f2e4d309de7
Reward Khe <rewardkhe@outlook.com>
2024-02-29 15:49:53 +1100

    client parameters changed

--- src/ConnectToXeroButton.jsx
+++ src/ConnectToXeroButton.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function ConnectToXeroButton() {
   const handleConnectToXero = () => {
     const clientId = 'EB99B7C7A5B6431A801C4D0FE4D56ED1';
-    const redirectUri = encodeURIComponent('https://pilkadaris.com.au/');
-    const scope = encodeURIComponent('openid profile email offline_access accounting.transactions accounting.contacts');
-    const state = 'YOUR_STATE'; // Generate a unique state value for each authorization request
+    const redirectUri = encodeURIComponent('https://xero728.netlify.app/');
+    const scope = encodeURIComponent('offline_access openid profile email accounting.transactions');
+    const state = '123'; // Generate a unique state value for each authorization request
     const authorizationUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}`;
 
     window.location.href = authorizationUrl;
